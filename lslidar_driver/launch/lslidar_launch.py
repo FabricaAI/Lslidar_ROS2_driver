77e014598187b1ca9179e0ed125e45c288014843
Caleb Yoong <caleb.yoong216@gmail.com>
2025-10-15 11:38:42 +0800

    chore(lidar) formatting, configs

--- lslidar_driver/launch/lslidar_launch.py
+++ lslidar_driver/launch/lslidar_launch.py
@@ -9,32 +9,25 @@ from launch.actions import DeclareLaunchArgument
 import lifecycle_msgs.msg
 import os
 
+
 def generate_launch_description():
 
-    driver_dir = os.path.join(get_package_share_directory('lslidar_driver'), 'params', 'lsx10.yaml')
-                     
-    driver_node = LifecycleNode(package='lslidar_driver',
-                                executable='lslidar_driver_node',
-                                name='lslidar_driver_node',		#设置激光数据topic名称
-                                output='screen',
-                                emulate_tty=True,
-                                namespace='',
-                                parameters=[driver_dir],
-                                )
+    driver_dir = os.path.join(
+        get_package_share_directory("lslidar_driver"), "params", "lsx10.yaml"
+    )
 
+    driver_node = LifecycleNode(
+        package="lslidar_driver",
+        executable="lslidar_driver_node",
+        name="lslidar_driver_node",  # 设置激光数据topic名称
+        output="screen",
+        emulate_tty=True,
+        namespace="",
+        parameters=[driver_dir],
+    )
 
-    rviz_dir = os.path.join(get_package_share_directory('lslidar_driver'), 'rviz', 'lslidar.rviz')
-
-    rviz_node = Node(
-        package='rviz2',
-        namespace='',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', rviz_dir],
-        output='screen')
-
-    return LaunchDescription([
-        driver_node,
-        rviz_node,
-    ])
-
+    return LaunchDescription(
+        [
+            driver_node,
+        ]
+    )
