c086516c5196b425dfc096ddb49504023130aa5a
hermanthong <hermanthong@gmail.com>
2025-10-21 10:36:41 +0800

    code from baidu

--- lslidar_driver/src/lslidar_driver_node.cc
+++ lslidar_driver/src/lslidar_driver_node.cc
@@ -15,20 +15,21 @@
  * along with the driver.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "lslidar_driver/lslidar_driver.h"
 #include "rclcpp/rclcpp.hpp"
+#include "lslidar_driver/lslidar_driver.h"
 
 using namespace lslidar_driver;
 volatile sig_atomic_t flag = 1;
 
-int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<lslidar_driver::LslidarDriver>();
-
-    while (rclcpp::ok() && node->polling()) {
+int main(int argc, char* argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<lslidar_driver::LslidarDriver>();
+  
+  while (rclcpp::ok() && node->polling()) {
         rclcpp::spin_some(node);
-    }
-    // rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+  }
+  //rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
 }
